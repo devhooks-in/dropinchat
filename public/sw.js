bff6c828710c64cac9184188187e7abbe19a7311
Dev Hooks <devhooks.in@gmail.com>
2025-07-04 16:08:53 +0000

    Add chat icon as a favicon and app main icon which you can use for PWA a

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,32 @@
-// Basic service worker for PWA caching
+// A basic service worker for offline functionality
+
 const CACHE_NAME = 'dropinchat-cache-v1';
 const urlsToCache = [
   '/',
-  '/manifest.json'
+  '/manifest.json',
 ];
 
 self.addEventListener('install', (event) => {
+  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       }
-      return fetch(event.request);
-    })
+    )
   );
 });
