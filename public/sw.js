66f56e3496c6f46b9a7fc05f1dc7f2fe4d0f1c87
Dev Hooks <devhooks.in@gmail.com>
2025-07-04 17:06:02 +0000

    web app should be installable

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,57 @@
-// A basic service worker for offline functionality
-
 const CACHE_NAME = 'dropinchat-cache-v1';
 const urlsToCache = [
   '/',
   '/manifest.json',
+  '/favicon.ico'
 ];
 
-self.addEventListener('install', (event) => {
-  // Perform install steps
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
+  // We only want to cache GET requests.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
+    caches.open(CACHE_NAME).then(async (cache) => {
+      const cachedResponse = await cache.match(event.request);
+      
+      const fetchedResponsePromise = fetch(event.request).then((networkResponse) => {
+        // Don't cache non-OK responses
+        if (networkResponse.ok) {
+            cache.put(event.request, networkResponse.clone());
         }
-        return fetch(event.request);
-      }
-    )
+        return networkResponse;
+      }).catch(() => {
+          // fetch failed, maybe offline
+          return cachedResponse; // return cached response if fetch fails
+      });
+      
+      return cachedResponse || fetchedResponsePromise;
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
